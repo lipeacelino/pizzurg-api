1460e1264497a1812df94c028742cf1f78659079
Felipe Acelino <felipe.acelino@outlook.com>
2023-03-14 05:51:03 -0300

    initial commit

---
+++ src/main/java/com/pizzurg/api/PizzurgApiApplication.java
@@ -0,0 +1,13 @@
+package com.pizzurg.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzurgApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzurgApiApplication.class, args);
+	}
+
+}
