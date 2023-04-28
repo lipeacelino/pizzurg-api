7f8dfd58e6801d385f6122de492926fa26c3d902
Felipe Acelino <felipe.acelino@outlook.com>
2023-04-28 03:43:00 -0300

    strategy removal

--- src/main/java/com/pizzurg/api/service/strategy/product/ProductUpdate.java
+++
@@ -1,8 +0,0 @@
-package com.pizzurg.api.service.strategy.product;
-
-import com.pizzurg.api.dto.input.product.UpdateProductDto;
-import com.pizzurg.api.entity.Product;
-
-public interface ProductUpdate {
-    void update(Product product, UpdateProductDto updateProductDto);
-}
